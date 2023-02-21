e47e3543ee6b55d1f9e9c110911929a5e41b12a5
Rodrigo De la Mora <84042065+rodrigodlmh@users.noreply.github.com>
2023-02-21 13:45:22 -0600

    Add files via upload

---
+++ Minesweeper/VSProject/VSProject/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿//----------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="😹👍">
+//     Company copyright tag.
+// </copyright>
+//----------------------------------------------------------------------
+namespace VSProject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public partial class App : Application
+    {
+    }
+}
