f50500d322ff7d28eb10c88c25ecf51b2511714d
Rodrigo De la Mora <84042065+rodrigodlmh@users.noreply.github.com>
2023-02-23 19:37:08 -0600

    Add files via upload

---
+++ VSProject/VSProject/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿//----------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="😹👍">
+//     Company copyright tag.
+// </copyright>
+//----------------------------------------------------------------------
+namespace VSProject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// click on start button event
+        /// </summary>
+        /// <param name="sender"> a sender</param>
+        /// <param name="e"> a letter</param>
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a different window based on the difficulty selected
+            // Right know all options open the medium difficulty window
+            int columns;
+            if (int.TryParse(columnsTB.Text, out columns))
+            {
+                int rows;
+                if (int.TryParse(rowsTB.Text, out rows))
+                {
+                    int mines;
+                    if (int.TryParse(minesTB.Text, out mines))
+                    {
+                        if (columns >= 1 && columns <= 32 && rows >= 1 && rows <= 22)
+                        {
+                            if ((rows * columns) >= mines)
+                            {
+                                Minesweeper window;
+                                if (safteynetCB.IsChecked == true)
+                                {
+                                    window = new Minesweeper(rows, columns, mines, true, playerCB.SelectedIndex);
+                                }
+                                else 
+                                {
+                                    window = new Minesweeper(rows, columns, mines, false, playerCB.SelectedIndex);
+                                }
+
+                                window.Show();
+                                this.Close();
+                            }
+                            else
+                            {
+                                ////mines are too big
+                                MessageBox.Show("Invalid number of mines", "Mine issue 🤯");
+                            }
+                        }
+                        else
+                        {
+                            ////Columns or rows are not in the parameters of numbers
+                            MessageBox.Show("You need to have the rows and columns more than zero and less than 100 🤯", "board issue");
+                        }
+                    }
+                    else
+                    {
+                        // enter a valid number of mines
+                        MessageBox.Show("You need to have the mines number more than zero 🤯", "mine issue");
+                    }
+                }
+                else
+                {
+                    // enter a valid number of rows
+                    MessageBox.Show("Your rows need to be a number. 🤯", "row issue");
+                }
+            }
+            else
+            {
+                // enter a valid number of columns
+                MessageBox.Show("Your columns need to be a number. 🤯", "column issue");
+            }
+        }
+
+        /// <summary>
+        /// select game difficulty (easy, medium, hard)
+        /// </summary>
+        /// <param name="sender"> a sender</param>
+        /// <param name="e"> a letter</param>
+        private void Difficulty_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Set selection variable based on what the user chose in the menu
+            string selection = Difficulty.SelectedItem.ToString();
+            if (selection == "System.Windows.Controls.Button: Easy")
+            {
+                columnsTB.Text = "8";
+                rowsTB.Text = "8";
+                minesTB.Text = "10";
+            }
+            else if (selection == "System.Windows.Controls.Button: Medium")
+            {
+                columnsTB.Text = "16";
+                rowsTB.Text = "16";
+                minesTB.Text = "40";
+            }
+            else if (selection == "System.Windows.Controls.Button: Hard")
+            {
+                columnsTB.Text = "30";
+                rowsTB.Text = "16";
+                minesTB.Text = "99";
+            }
+        }
+
+        /// <summary>
+        /// Exit Button to close to the WPF 
+        /// </summary>
+        /// <param name="sender"> a sender</param>
+        /// <param name="e"> a letter</param>
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// About the Developers message box and game version and the date
+        /// </summary>
+        /// <param name="sender"> a sender</param>
+        /// <param name="e"> a letter</param>
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Developers:\n\t Rodrigo De la Mora,\n\t Damian Olivo, \n\tDonald Swearingen,\n\t Sam Thorpe  \nVersion: 1.1.1 \nDate: 2/7/2023 \nLists used: 7 \nArrays used: 2");          
+        }
+
+        /// <summary>
+        /// button to show the rules of the game  
+        /// </summary>
+        /// <param name="sender"> a sender</param>
+        /// <param name="e"> a letter</param>
+        private void RulesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Minesweeper Rules: \n\n" +
+                "1. The goal is to uncover all cells that do not contain mines. \n" +
+                "2. Left-click to uncover a cell. If the cell contains a mine, you lose.\n" +
+                "3. Right-click to question-mark a cell if you think it contains a mine.\n" +
+                "4. Right-click again to flag a cell if you are sure it contains a mine.\n" +
+                "5. The number on a cell indicates how many mines are in the surrounding 8 cells.\n" +
+                "6. Uncover all cells that do not contain mines to win the game.\n" +
+                "7. Be careful, one incorrect move can result in a loss!");
+        }
+    }
+}
---
+++ VSProject/VSProject/MinesweeperGPT.cs
@@ -0,0 +1,153 @@
+﻿//----------------------------------------------------------------------
+// <copyright file="MinesweeperGPT.cs" company="😹👍">
+//     copyright  header 
+// </copyright>
+//----------------------------------------------------------------------
+namespace VSProject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// the computer player
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public class MinesweeperGPT
+    {
+        /// <summary>
+        /// the minefield for the user to interact with to be able to play 
+        /// </summary>
+        public Minefield GameMinefield;
+
+        /// <summary>
+        /// the coordiates that the computer player has already revealed are going to be stored in a list 
+        /// </summary>
+        public List<Coordinate> RevealedCoordinates;
+
+        /// <summary>
+        /// the coordinate that is being checked/used 
+        /// </summary>
+        public Coordinate CurrentCoordinate;
+
+        /// <summary>
+        /// the coordinates of the squares surrounding a single square
+        /// </sumamry>
+        public List<Coordinate> SurroundingCoordinates;
+
+        /// <summary>
+        /// a array of all the flag coordinates the computer player has placed
+        /// </summary>
+        private Coordinate[] Flags;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinesweeperGPT"/> class 
+        /// </summary>
+        /// <param name="mf"> the minefield</param>
+        public MinesweeperGPT(Minefield mf)
+        {
+            GameMinefield = mf;
+            RevealedCoordinates = new List<Coordinate>();
+        }
+
+        /// <summary>
+        /// Gets random coordinate to click
+        /// </sumamry>
+        /// <param name="coordinate"> chooses a coordinate to pick and interact with</param>
+        public Coordinate RandomizeSelection()
+        {
+            bool valid = false;
+            while(!valid)    
+            {
+                Coordinate selection = Coordinate.GetRandomCoordinate(GameMinefield.Columns, GameMinefield.Rows);
+                if (GameMinefield.Squares[selection.X, selection.Y].Revealed)
+                {
+                    valid = false;
+                }
+                else
+                {
+                    RevealedCoordinates.Add(selection);
+                    GameMinefield.Squares[selection.X, selection.Y].Revealed = true;
+                    return selection;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// the selection of how smart the computer player is going to be 
+        /// </summary>
+        /// <returns> a coordinate</returns>
+        public Coordinate SmartSelection()
+        {
+            return null;
+        }
+
+        
+        private void Spread(Coordinate coordinate)
+        {
+        int grassCount = 0;
+        int stateNumber;
+            /*
+            Coordinate A = new coordinate(coordinate.X - 1, coordinate.Y - 1);
+            Coordinate B = new coordinate(coordinate.X, coordinate.Y - 1);
+            Coordinate C = new coordinate(coordinate.X + 1, coordinate.Y - 1);
+            Coordinate D = new coordinate(coordinate.X - 1, coordinate.Y);
+            Coordinate E = new coordinate(coordinate.X + 1, coordinate.Y);
+            Coordinate F = new coordinate(coordinate.X - 1, coordinate.Y + 1);
+            Coordinate G = new coordinate(coordinate.X, coordinate.Y + 1);
+            Coordinate H = new coordinate(coordinate.X + 1, coordinate.Y + 1);
+            
+            
+            if (this.GameMinefield.IsCoordinateValid(coordinate))
+                {
+                    for (int xNum = coordinate.X - 1; xNum <= coordinate.X + 1; xNum++)
+                    {
+                        for (int yNum = coordinate.Y - 1; yNum <= coordinate.Y + 1; yNum++)
+                        {
+                            // if outside Minefield area BORDERS!! OR ALREADY REVEALED
+                            if (this.GameMinefield.IsCoordinateValid(new Coordinate(xNum, yNum)))
+                            {
+                                if (this.GameMinefield.Squares[xNum, yNum].Revealed != true)
+                                { 
+                                    if(surroudingNum(Coordinate coordinate) == stateNum){
+                                        try to flag everything around itself
+                                        add the coordinates to the list
+                                    }
+                                    else{
+                                        call itself with any surronding numbers
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }*/
+            /*
+             * method that gets the number of coordinates that isn't revealed returns a number 
+             * */
+
+            /*
+             * 
+             * */
+        }
+
+        public int SurroundingNumbers(Coordinate coordinate)
+        {
+            int number = 0;
+            for (int xNum = coordinate.X - 1; xNum <= coordinate.X + 1; xNum++)
+            {
+                for (int yNum = coordinate.Y - 1; yNum <= coordinate.Y + 1; yNum++)
+                {
+                    /*State
+                    if (this.GameMinefield.Squares[xNum, yNum].Revealed != true || this.Game.Minefield.Squares[xNum, yNum].State2 == State2.Flagged)
+                    {
+                        
+                    }*/
+                }
+            }
+            return number;
+        }
+    }
+}
